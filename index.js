be3e5921892c62386b7f3f95c498769831295bcd
bibhu-git <bibhuranjanm324@gmail.com>
2025-04-08 02:25:11 +0530

    add core credential

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 DBCONNECT();
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use('/api/user',userRouter);
 app.use('/api/admin',adminRouter);
 app.use('/images',express.static("uploads"));
