8f9d515bbd5d53a92c71a4fc37856f399b0e3b4b
bibhu-git <bibhuranjanm324@gmail.com>
2025-04-08 16:05:52 +0530

    fix core error

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const port = process.env.PORT || 3000;
 DBCONNECT();
 
 app.use(bodyParser.json());
-app.use(cors({ origin: "canteen-management-delta.vercel.app", credentials: true }));
+app.use(cors({
+    origin: "https://canteen-management-delta.vercel.app",
+    credentials: true
+  }));
+  
 app.use('/api/user',userRouter);
 app.use('/api/admin',adminRouter);
 app.use('/images',express.static("uploads"));
